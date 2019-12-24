2510887d0645f2155fa68077e803940c32c6746b
utrijezadiminje <stanojevic.dimitrije@gmail.com>
2019-12-24 18:03:07 +0100

    login umrela mi majka radim dizajn sad

--- app/src/main/java/com/example/helpcare/FirstRunCheck.java
+++ app/src/main/java/com/example/helpcare/FirstRunCheck.java
@@ -3,26 +3,28 @@ package com.example.helpcare;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 public class FirstRunCheck extends AppCompatActivity {
 
+    SharedPreferences prefs = null;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_first_run_check);
-        Boolean isFirstRun = getSharedPreferences("PREFERENCE", MODE_PRIVATE)
-                .getBoolean("isFirstRun", true);
-
-        if (isFirstRun) {
-            //show start activit
-
-            startActivity(new Intent(FirstRunCheck.this, LoginActivity.class));
-
-        }
-
-
-        getSharedPreferences("PREFERENCE", MODE_PRIVATE).edit()
-                .putBoolean("isFirstRun", false).commit();
+        // Perhaps set content view here
+        prefs = getSharedPreferences("com.example.helpcare", MODE_PRIVATE);
     }
-}
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (prefs.getBoolean("firstrun", true)) {
+            Intent intent = new Intent(this,LoginActivity.class);
+            prefs.edit().putBoolean("firstrun", false).apply();
+        }
+    }
+    }
--- app/src/main/java/com/example/helpcare/LoginActivity.java
+++ app/src/main/java/com/example/helpcare/LoginActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -17,14 +18,21 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Name = (EditText)findViewById(R.id.etName);
-        Password= (EditText)findViewById(R.id.etPassword);
-        Login =(Button)findViewById(R.id.btnLogin);
+        Name = findViewById(R.id.etName);
+        Password=findViewById(R.id.etPassword);
+        Login =findViewById(R.id.btnLogin);
 
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                val(Name.getText().toString(),Password.getText().toString());
+            }
+        });
     }
-    private void val(String iN,String iP,String userN,String userP){
-        if(userN==iN && userP==iP) {
+    private void val(String userN,String userP){
+        if((userN.equals("Admin")) && (userP.equals("123"))) {
             Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
         }
     }
 }
