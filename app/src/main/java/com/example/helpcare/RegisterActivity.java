e366a67a10a3b6877803273222786fbe6d5c0f83
nmitrovic002 <nmitrovic002@gmail.com>
2019-12-24 23:32:46 +0100

    dicko ciganin

---
+++ app/src/main/java/com/example/helpcare/RegisterActivity.java
@@ -0,0 +1,22 @@
+package com.example.helpcare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private EditText Username;
+    private EditText Password;
+    private EditText Password2;
+    private Button Register;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+    }
+}
