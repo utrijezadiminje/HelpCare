393e845a3376c41d11fec86e76b9ee8d40bffc29
utrijezadiminje <stanojevic.dimitrije@gmail.com>
2020-01-14 14:58:06 +0100

    reg spiner

--- app/src/main/java/com/example/helpcare/RegisterActivity.java
+++ app/src/main/java/com/example/helpcare/RegisterActivity.java
@@ -3,8 +3,13 @@ package com.example.helpcare;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -12,12 +17,26 @@ public class RegisterActivity extends AppCompatActivity {
     private EditText Password;
     private EditText Password2;
     private Button Register;
-
-
+    private Spinner spinner;
+    private String tip;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        spinner=findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter= ArrayAdapter.createFromResource(this,R.array.tipprofila,android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                tip = adapterView.getItemAtPosition(i).toString();
+            }
 
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
 }
