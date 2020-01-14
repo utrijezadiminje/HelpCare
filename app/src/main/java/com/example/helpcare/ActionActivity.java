4eeb7ecaf329454eb3544eecaadb0b76cd47a1db
nmitrovic002 <nmitrovic002@gmail.com>
2020-01-14 14:37:22 +0100

    dicko ciganin

---
+++ app/src/main/java/com/example/helpcare/ActionActivity.java
@@ -0,0 +1,14 @@
+package com.example.helpcare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_action);
+    }
+}
