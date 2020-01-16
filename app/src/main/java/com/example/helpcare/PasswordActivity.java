459db8a09dd0aeb1e6eb9af1a91da683678bd964
nmitrovic002 <nmitrovic002@gmail.com>
2020-01-16 14:15:09 +0100

    povezani passwordAct i userAct i dicko ciganin

--- app/src/main/java/com/example/helpcare/PasswordActivity.java
+++ app/src/main/java/com/example/helpcare/PasswordActivity.java
@@ -3,6 +3,8 @@ package com.example.helpcare;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -11,32 +13,40 @@ public class PasswordActivity extends AppCompatActivity {
     private EditText oldPassword;
     private EditText Password1;
     private EditText Password2;
+    private Button Change;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_password);
         oldPassword = findViewById(R.id.oldPassword);
-        Password1=findViewById(R.id.newPassword1);
-        Password2=findViewById(R.id.newPassword2);
-        String[] StariPass = new String[]{"1", "2", "3"}; //zameni sa lozinkom iz baze
-        if (Password1.equals(Password2) && StariPass.equals(oldPassword)){
-            //zameni Password
-        }
-        else if(!Password1.equals(Password2)){
-            CharSequence text = "LOZINKE SE NE POKLAPAJU";
-            Toast toast = Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT);
-            toast.show();
-        }
-        else if(!StariPass.equals(oldPassword)){
-            CharSequence text = "STARA LOZINKA SE NE POKLAPA";
-            Toast toast = Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT);
-            toast.show();
-        }
-        else{
-            CharSequence text = "DOŠLO JE DO GREŠKE";
-            Toast toast = Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT);
-            toast.show();
-        }
+        Password1 = findViewById(R.id.newPassword1);
+        Password2 = findViewById(R.id.newPassword2);
+
+        Change = findViewById(R.id.btnReset);
+        Change.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String Pass1 = Password1.getText().toString();
+                String Pass2 = Password2.getText().toString();
+                String OldPass = oldPassword.getText().toString();
+                if (Pass1.equals(Pass2) && OldPass.equals("123")) {
+                    //zameni Password
+                    //123 zameni za lozinkom iz baze
+                } else if (!Pass1.equals(Pass2)) {
+                    CharSequence text = "LOZINKE SE NE POKLAPAJU";
+                    Toast toast = Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT);
+                    toast.show();
+                } else if (!OldPass.equals("123")) {
+                    CharSequence text = "STARA LOZINKA SE NE POKLAPA";
+                    Toast toast = Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT);
+                    toast.show();
+                } else {
+                    CharSequence text = "DOŠLO JE DO GREŠKE";
+                    Toast toast = Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/helpcare/UserActivity.java
+++ app/src/main/java/com/example/helpcare/UserActivity.java
@@ -2,13 +2,26 @@ package com.example.helpcare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class UserActivity extends AppCompatActivity {
+
+    private Button password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
+        password = findViewById(R.id.button2);
+        password.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(UserActivity.this, PasswordActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
